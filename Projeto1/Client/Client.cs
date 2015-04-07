ea7820b5333453d2c1a2c2e63ce8b5b742584abb
CriCa <ei10041@fe.up.pt>
2015-04-07 20:35:20 +0100

    Created solution :)

---
+++ Projeto1/Client/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Remoting;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
---
+++ Projeto1/Server/Server.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Remoting;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+    }
+}
