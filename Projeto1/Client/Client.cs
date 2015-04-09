3514af57604178af40b74914ba78dedd8a2d87d2
CriCa <ei10041@fe.up.pt>
2015-04-09 16:28:20 +0100

    minor changes

--- Projeto1/Client/Client.cs
+++ Projeto1/Client/Client.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 
-
 class Client
 {
     [STAThread]
--- Projeto1/DiginoteManager/DiginoteDatabase.cs
+++ Projeto1/DiginoteManager/DiginoteDatabase.cs
@@ -15,4 +15,3 @@ public class DiginoteDatabase
         diginotesOwners.Add(dig, user);
     }
 }
-
