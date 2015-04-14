c5e923ea0b46e51f8c5a7aaf3cceac0247aba6ab
CriCa <ei10041@fe.up.pt>
2015-04-14 05:43:26 +0100

    started main window

--- Projeto1/Client/View/MainWindow.xaml.cs
+++ Projeto1/Client/View/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Messaging;
+using Client.Helpers;
 
 namespace Client.View
 {
@@ -22,6 +24,21 @@ namespace Client.View
         public MainWindow()
         {
             InitializeComponent();
+
+            Messenger.Default.Register<NotificationMessage<NotificationType>>(this, NotificationMessageHandler);
+        }
+
+        private void NotificationMessageHandler(NotificationMessage<NotificationType> msg)
+        {
+            /*if (msg.Content.Type == NotifType.LOGIN)
+            {
+                // open main window
+                MainWindow main = new MainWindow();
+                main.Show();
+
+                // close current
+                Close();
+            }*/
         }
     }
 }
---
+++ Projeto1/Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Helpers;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace Client.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+
+    }
+}
